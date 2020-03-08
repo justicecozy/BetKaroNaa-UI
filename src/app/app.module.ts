192ad820b40d538e32885e743ce75fa3746d88fb
tech-somish <contact@somish.com>
2020-03-08 11:18:06 +0530

    bind metamask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CountdownModule } from 'ngx-countdown';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,CountdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
